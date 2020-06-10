dd1b83f0e1f136ca8df1b7d8b91befce1fcd631d
rishav-dev <rkdx007@rediffmail.com>
2020-06-10 14:58:42 +0530

    Final Done

--- Assignment2/som.py
+++ Assignment2/som.py
@@ -1,15 +1,15 @@
 import SimpSOM as som
 import numpy as np
 
-filename = 'Data/pendigits.tra'
+filename = 'Data/pendigits.tes'
 raw_data = open(filename, 'rt')
 data = np.loadtxt(raw_data, delimiter=",")
-data_without_label = data[:2000,:16]
-labels = data[:2000,16]
+data_without_label = data[:,:16]
+labels = data[:,16]
 
 #30x30 SOM with periodic boundary conditions
 net = som.somNet(30, 30, data_without_label, PBC=True)
-net.train(0.01, 1000)
+net.train(0.01, 10000)
 net.save('weights')
 net.nodes_graph(colnum=0)
 net.diff_graph()
